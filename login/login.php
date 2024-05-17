749efdd99951e80af01ea14d367b5d8f99f423db
nischal12aryal <119328699+nischal12aryal@users.noreply.github.com>
2024-05-17 22:41:29 +0545

    login

--- login/login.php
+++ login/login.php
@@ -11,7 +11,7 @@ if (isset($_POST["login"])) {
     if ($user) {
         if (password_verify($password, $user["password"])) {
             $_SESSION["user"] = "yes";
-            header("Location: userloggedin.php"); // Redirect after successful login
+            header("Location: userloggedin.html"); // Redirect after successful login
             die(); // Stop further execution
         } else {
             echo "<div class='alert alert-danger'>Password does not match</div>";
@@ -40,7 +40,7 @@ if (isset($_POST["login"])) {
 
     <nav class="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
         <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-            <a href="index.html" class="flex items-center space-x-3 rtl:space-x-reverse">
+            <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
                 <!-- <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo"> -->
                 <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Vroom</span>
             </a>
@@ -61,20 +61,20 @@ if (isset($_POST["login"])) {
                 <ul
                     class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800">
                     <li>
-                        <a href="index.html"
+                        <a href="#"
                             class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
                             aria-current="page">Home</a>
                     </li>
                     <li>
-                        <a href="about.html"
+                        <a href="#"
                             class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
                     </li>
                     <li>
-                        <a href="services.html"
+                        <a href="#"
                             class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
                     </li>
                     <li>
-                        <a href="inquiry.html"
+                        <a href="#"
                             class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Inquiry</a>
                     </li>
                 </ul>
